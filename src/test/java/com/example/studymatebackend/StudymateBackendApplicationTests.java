844497ab4158e3b0f3dd6431d2067b2bcb574f50
5s-mini <dnclsehd123@naver.com>
2025-11-04 20:04:50 +0900

    "프로젝트 파일 생성"

---
+++ src/test/java/com/example/studymatebackend/StudymateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studymatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudymateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
